061376b7b732b1f8387c9cce5d1bc4c20bcf7e9a
Sohrabirad <bi.sohrabirad@gmail.com>
2025-05-24 12:33:34 +0330

    Initial commit

--- main.py
+++ main.py
@@ -35,5 +35,6 @@ async def websocket_endpoint(websocket: WebSocket):
         print("Client disconnected")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080)) 
     uvicorn.run(app, host="0.0.0.0", port=port)
+
